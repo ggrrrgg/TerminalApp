5c9aae614852aabbd18a3358482d11aa9f9d26a8
ggrrrgg <george.sheridan@gmail.com>
2023-05-02 13:44:45 +1000

    search function added, still some debugging todo

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 from mcontact_functions import search_mcontact, add_mcontact, update_mcontact, remove_mcontact, browse_mcontact
+import csv
 
 print('Hi, welcome to your Musician Contacts')
 
-file_name = 'contacts.csv'
+contact_master = 'contacts.csv'
 
 try:
-    mcontact_file = open(file_name, 'r')
-
+    mcontact_file = open(contact_master, 'r')
     mcontact_file.close()
 
 except FileNotFoundError as e:
-    mcontact_file = open(file_name, 'w')
+    mcontact_file = open(contact_master, 'w')
     mcontact_file.write('Name, Phone, Email, Instrument, City\n')
     mcontact_file.close()
         
 
 def create_menu():
+    
     print('1. Press 1 to Search a Contact')
     print('2. Press 2 to Add a New Contact')
     print('3. Press 3 to Update a Contact')
     print('4. Press 4 to Remove a Contact')
     print('5. Press 5 to Browse Contacts')
     print('6. Press 6 to Exit')
+    
     choice = input('Enter your Selection: ')
     return choice
 
@@ -31,15 +33,15 @@ while user_choice != '6':
     user_choice = create_menu()
 
     if (user_choice == '1'):
-        pass
+        search_mcontact(contact_master)
     elif (user_choice == '2'):
-        add_mcontact(file_name)
+        add_mcontact(contact_master)
     elif (user_choice == '3'):
-        update_mcontact(file_name)
+        update_mcontact(contact_master)
     elif (user_choice == '4'):
-        remove_mcontact(file_name)
+        remove_mcontact(contact_master)
     elif (user_choice == '5'):
-        browse_mcontact(file_name)
+        browse_mcontact(contact_master)
     elif (user_choice == '6'):
         continue
     else:
--- mcontact_functions.py
+++ mcontact_functions.py
@@ -1,9 +1,37 @@
 import csv
 
-def search_mcontact(file_name):
-    pass
-
-def add_mcontact(file_name):
+def search_mcontact(contact_master):
+    
+    search_name = input('Search by Name? (y/n): ')
+    if search_name == 'y':
+        name_lookup = input('Enter the Name of the Contact: ')
+        with open(contact_master, 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if (name_lookup == row[0]):
+                    print(row)
+                    return
+                
+    else:
+        search_instr = input('Search by Instrument/City? (y/n): ')
+        if search_instr != 'y':
+            print('Returning to Main Menu')
+            
+        else:
+            instr_lookup = input('What kind of Instrumentalist do you need? ')
+            city_lookup = input('Which city should they be based? ')
+            with open(contact_master, 'r') as f:
+                reader = csv.reader(f)
+                match_count = 0
+                for row in reader:
+                    if(instr_lookup == row[3] and city_lookup == row[4]):
+                        print(row)
+                        match_count += 1
+                if match_count == 0:
+                    print('Sorry, no matches for that instrument in that location')
+                        
+                    
+def add_mcontact(contact_master):
     
     while True:
         print('Adding New Contact...')
@@ -12,23 +40,23 @@ def add_mcontact(file_name):
         mcontact_email = input('Enter email: ')
         mcontact_instr = input('Instrument / job: ')
         mcontact_city = input('Enter city: ')
-        with open(file_name, 'a') as mcontact_file:
+        with open(contact_master, 'a') as mcontact_file:
             writer = csv.writer(mcontact_file)
             writer.writerow([mcontact_name, mcontact_phone, mcontact_email, mcontact_instr, mcontact_city])
         print('Contact Added')
         another = input('Would you like to Add another Contact? (y/n): ')
         if another != 'y':
-            break
+            break     
 
+def update_mcontact(contact_master):
     
-def update_mcontact(file_name):
-    
+    # browse_mcontact(file_name)
     while True:
         mcontact_name = input('Enter the name of the Contact to Update: ')
         mcontact_lists = []
         
-        with open(file_name, 'r') as mcontact_file:
-            reader = csv.reader(mcontact_file)
+        with open(contact_master, 'r') as f:
+            reader = csv.reader(f)
             for row in reader:
                 if (mcontact_name == row[0]):
                     mcontact_lists.append(row)
@@ -40,43 +68,56 @@ def update_mcontact(file_name):
         for row in mcontact_lists:
             if mcontact_update in row:
                 row[row.index(mcontact_update)] = mcontact_new_detail
-
+        
+        with open(contact_master, 'r') as f:
+            reader = csv.reader(f)
+            rows = [row for row in reader if row[0] != mcontact_name] + mcontact_lists
+        
+        with open(contact_master, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerows(rows)
+        
         print('Updating...')
         print(mcontact_lists)
         print('Contact Updated')
         
         another = input('Would you like to Update another Contact? (y/n): ')
         if another != 'y':
-            break        
+            break
 
-def remove_mcontact(file_name):
-
+def remove_mcontact(contact_master):
+    
     while True: 
         print('Remove Contact...')
         mcontact_name = input('Enter the name of the contact you want to remove: ')
         mcontact_lists = []
         
-        with open(file_name, 'r')as mcontact_file:
-            reader = csv.reader(mcontact_file)
+        with open(contact_master, 'r')as f:
+            reader = csv.reader(f)
             for row in reader:
                 if not row:
                     continue
                 if(mcontact_name != row[0]):
                     mcontact_lists.append(row)
-        print(mcontact_lists)
+        # print(mcontact_lists)
 
-        with open(file_name, 'w')as mcontact_file:
-            writer = csv.writer(mcontact_file)
+        with open(contact_master, 'w')as f:
+            writer = csv.writer(f)
             writer.writerows(mcontact_lists)
         print('Contact Removed')
+        
         another = input('Would you like to Remove another Contact? (y/n): ')
         if another != 'y':
             break
 
-def browse_mcontact(file_name):
+def browse_mcontact(contact_master):
+    
     print('Displaying Contacts...')
-    with open(file_name, 'r')as reader:
+    
+    with open(contact_master, 'r')as reader:
         csv.reader = reader
         for row in csv.reader:
             print(row)
+
+   
     
