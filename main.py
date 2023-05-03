1b7473f84f45878e6521d0db069cd3b54c25efdc
ggrrrgg <george.sheridan@gmail.com>
2023-05-03 20:33:23 +1000

    progress bars added, more error corection WIP

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from mcontact_functions import search_mcontact, add_mcontact, update_mcontact, remove_mcontact, browse_mcontact
 from rich import print
 from rich.console import Console
+from art import *
 
 console = Console()
 
 console.print(':guitar:'' Hi, welcome to your Musician Contacts '':guitar:', style='bold underline red on light_goldenrod1')
 
+print()
 contact_master = 'contacts.csv'
 
 try:
@@ -19,14 +21,14 @@ except FileNotFoundError as e:
         
 
 def create_menu():
-    
+    print()
     console.print('Press 1 to [bold cyan]Search[/] a Contact')
     console.print('Press 2 to [bold green]Add[/] a New Contact')
     console.print('Press 3 to [bold purple]Update[/] a Contact')
     console.print('Press 4 to [bold red]Remove[/] a Contact')
     console.print('Press 5 to [bold dark_orange]Browse[/] Contacts')
     console.print('Press 6 to [bold yellow]Exit[/]')
-    
+    print()
     choice = input('Enter your Selection: ')
     return choice
 
@@ -53,5 +55,7 @@ while user_choice != '6':
 
     input('Press Enter to continue...')
 
-console.print(':waving_hand:'' Thankyou bye bye '':waving_hand:', style='bold underline light_goldenrod1')
-
+print()
+console.print(':waving_hand:'' Thankyou for using... '':waving_hand:', style='bold underline light_goldenrod1 on red')
+print()
+tprint('BYE', 'rnd-medium')
--- mcontact_functions.py
+++ mcontact_functions.py
@@ -1,6 +1,8 @@
 import csv
 from rich import print
 from rich.console import Console
+import time
+from rich.progress import Progress
 
 console = Console()
 
@@ -9,8 +11,8 @@ def search_mcontact(contact_master):
     console.print('Search Contacts', style='bold cyan')
 
     search_name = input('Search by Name? (y/n): ')
-    if search_name == 'y':
-        name_lookup = input('Enter the Name of the Contact: ')
+    if search_name.lower() == 'y':
+        name_lookup = input('Enter the Name of the Contact: ').strip()
         with open(contact_master, 'r') as f:
             reader = csv.reader(f)
             found_match = False
@@ -24,17 +26,22 @@ def search_mcontact(contact_master):
                 
     else:
         search_instr = input('Search by Instrument/City? (y/n): ')
-        if search_instr != 'y':
+        if search_instr.lower() != 'y':
             console.print('Returning to Main Menu', style='bold cyan')
             
         else:
-            instr_lookup = input('What kind of Instrumentalist do you need? ')
-            city_lookup = input('In which City / Location? ')
+            instr_lookup = input('What kind of Instrumentalist do you need? ').strip()
+            city_lookup = input('In which City / Location? ').strip()
             with open(contact_master, 'r') as f:
                 reader = csv.reader(f)
                 match_count = 0
                 for row in reader:
                     if(instr_lookup == row[3] and city_lookup == row[4]):
+                        with Progress() as progress:
+                            task1 = progress.add_task("[cyan]Searching...", total=10)
+                            while not progress.finished:
+                                progress.update(task1, advance=0.6)
+                                time.sleep(0.1)
                         console.print(*row, style='bold cyan')
                         match_count += 1
                 if match_count == 0:
@@ -47,17 +54,17 @@ def add_mcontact(contact_master):
     
     while True:
         
-        mcontact_name = input('Enter name: ')
-        mcontact_phone = input('Enter ph number: ')
-        mcontact_email = input('Enter email: ')
-        mcontact_instr = input('Instrument / job: ')
-        mcontact_city = input('Enter city: ')
+        mcontact_name = input('Enter name: ').strip()
+        mcontact_phone = input('Enter ph number: ').strip()
+        mcontact_email = input('Enter email: ').strip()
+        mcontact_instr = input('Instrument / job: ').strip()
+        mcontact_city = input('Enter city: ').strip()
         with open(contact_master, 'a') as mcontact_file:
             writer = csv.writer(mcontact_file)
             writer.writerow([mcontact_name, mcontact_phone, mcontact_email, mcontact_instr, mcontact_city])
         console.print('Contact Added', style='bold green')
         another = input('Would you like to Add another Contact? (y/n): ')
-        if another != 'y':
+        if another.lower() != 'y':
             break     
 
 def update_mcontact(contact_master):
@@ -66,7 +73,7 @@ def update_mcontact(contact_master):
     browse_mcontact(contact_master)
 
     while True:
-        mcontact_name = input('Enter the name of the Contact to Update: ')
+        mcontact_name = input('Enter the name of the Contact to Update: ').strip()
         mcontact_lists = []
         
         with open(contact_master, 'r') as f:
@@ -81,8 +88,8 @@ def update_mcontact(contact_master):
                 console.print('Sorry, could not find a contact with that name', style='bold purple')
                 return
         
-        mcontact_update = input('Which Detail do you want to Update? ')
-        mcontact_new_detail = input('Enter the New Detail: ')
+        mcontact_update = input('Which Detail do you want to Update? ').strip()
+        mcontact_new_detail = input('Enter the New Detail: ').strip()
         
         for row in mcontact_lists:
             if mcontact_update in row:
@@ -96,13 +103,18 @@ def update_mcontact(contact_master):
             writer = csv.writer(f)
             writer.writerows(rows)
         
-        console.print('Updating...', style='bold purple')
+        with Progress() as progress:
+            task1 = progress.add_task("[purple]Updating...", total=10)
+            while not progress.finished:
+                progress.update(task1, advance=0.5)
+                time.sleep(0.1) 
+        
         for contact in mcontact_lists:
             console.print(*contact, style='bold cyan')
         console.print('Contact Updated', style='bold purple')
         
         another = input('Would you like to Update another Contact? (y/n): ')
-        if another != 'y':
+        if another.lower() != 'y':
             break
 
 def remove_mcontact(contact_master):
@@ -112,7 +124,7 @@ def remove_mcontact(contact_master):
     
     while True: 
         
-        mcontact_name = input('Enter the name of the contact you want to remove: ')
+        mcontact_name = input('Enter the name of the contact you want to remove: ').strip()
         mcontact_lists = []
         
         with open(contact_master, 'r')as f:
@@ -128,7 +140,7 @@ def remove_mcontact(contact_master):
         console.print(f'{mcontact_name} Removed', style='bold red')
         
         another = input('Would you like to Remove another Contact? (y/n): ')
-        if another != 'y':
+        if another.lower() != 'y':
             break
 
 def browse_mcontact(contact_master):
