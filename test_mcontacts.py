4d8b453f23aa5f13235e08cf319956e6dcb6b919
ggrrrgg <george.sheridan@gmail.com>
2023-05-07 12:00:24 +1000

    addcontact pytest passed

--- test_mcontacts.py
+++ test_mcontacts.py
@@ -1,7 +1,12 @@
 import pytest
+import csv
 from main import create_menu
+from mcontact_functions import add_mcontact
 from unittest.mock import patch
 import re
+
+# TEST create_menu function:
+# Press 6 to exit when menu is displayed to complete the test.
 
 def test_create_menu(capfd):
     with patch('builtins.input'):
@@ -20,6 +25,29 @@ def test_create_menu(capfd):
     out, err = capfd.readouterr()
     
     assert re.sub(r'\x1b\[\d+(;\d+)*m', '', expected_output) in re.sub(r'\x1b\[\d+(;\d+)*m', '', out)
+
+# TEST add_mcontacts function:
+
+contacts_test = 'contacts_test.csv'
+
+def test_add_mcontact(monkeypatch):
+    original_length = 0
+    
+    with open(contacts_test) as f:
+        reader = csv.reader(f)
+        original_length = sum(1 for row in reader)
+    monkeypatch.setattr('builtins.input', lambda _: 'entry 1')
+    add_mcontact(contacts_test)
+    
+    with open(contacts_test) as f:
+        reader = csv.reader(f)
+        new_length = sum(1 for row in reader)
+    
+    print(original_length)
+    print(new_length)
+    
+    assert new_length == original_length + 1
+
 
 
 
